8cc4e8a5a79d678665d78291eadd75668e0199c6
WhoShotYa <youngkibwe@gmail.com>
2025-05-06 22:36:19 +0200

    adding functionality to the frontend

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,15 @@ const nav = document.getElementById('nav');
 toggle.addEventListener('click', () => {
   nav.classList.toggle('show');
 });
+
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add("active");
+})
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove("active");
+})

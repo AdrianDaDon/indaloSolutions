ca66438529a7088b0e0a662c4e96c781e504d6c7
WhoShotYa <youngkibwe@gmail.com>
2025-05-12 09:09:56 +0200

    secured my data

--- src/js/login.js
+++ src/js/login.js
@@ -3,20 +3,20 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebas
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-analytics.js";
 import {
   getAuth,
-  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-auth.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: window.env.API_KEY,
-  authDomain:  window.env.AUTH_DOMAIN,
-  projectId:  window.env.PROJECT_ID,
-  storageBucket: window.env.STOROAGE_BUCKET,
-  messagingSenderId:  window.env.MESSAGING_SENDER_ID,
-  appId:  window.env.APP_ID,
-  measurementId:  window.env.MEASUREMENT_ID,
-};
+    apiKey: window.env.API_KEY,
+    authDomain:  window.env.AUTH_DOMAIN,
+    projectId:  window.env.PROJECT_ID,
+    storageBucket: window.env.STOROAGE_BUCKET,
+    messagingSenderId:  window.env.MESSAGING_SENDER_ID,
+    appId:  window.env.APP_ID,
+    measurementId:  window.env.MEASUREMENT_ID,
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -27,22 +27,16 @@ const loginBtn = document.getElementById("register");
 loginBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const email = document.getElementById("register-email").value;
-  const passwrd = document.getElementById("register-password").value;
-  const confirmPasswrd = document.getElementById("confirm-password").value;
+  const email = document.getElementById("email").value;
+  const passwrd = document.getElementById("password").value;
 
-  createUserWithEmailAndPassword(auth, email, passwrd)
+  signInWithEmailAndPassword(auth, email, passwrd)
     .then((userCredential) => {
       // Signed up
       const user = userCredential.user;
 
-      if(passwrd === confirmPasswrd){
-          alert("Creating Accout---");
-          window.location.href = "location of the next page";
-
-      }else{
-        alert("Passwords do not match");
-      }
+      alert("Sigin Account---");
+      window.location.href = "location of the next page";
     })
     .catch((error) => {
       const errorCode = error.code;

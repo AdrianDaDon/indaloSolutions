e9e621936b672e36b311352a8aa4186eb7c42411
WhoShotYa <youngkibwe@gmail.com>
2025-05-13 20:10:07 +0200

    working on functionality

--- src/js/finder.js
+++ src/js/finder.js
@@ -2,6 +2,8 @@ const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
 const resultsGrid = document.getElementById("results-grid");
 const messageArea = document.getElementById("message-area");
+const placeholderImage =
+  "https://furntech.org.za/wp-content/uploads/2017/05/placeholder-image.png";
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -16,8 +18,20 @@ searchForm.addEventListener("submit", (e) => {
 
 async function searchProduct(query) {
   showMessage(`Searching for "${query}..."`, false, true);
+  resultsGrid.innerHTML = "";
   const dataset = await fetch("./assets/ingredients.json")
-    .then((data) => console.log(data.json()))
+    .then((response) => response.json())
+    .then((data) => {
+      try {
+        if (data) {
+          displayProducts(data, query.toLowerCase());
+        } else {
+          showMessage(`"${query}" does not exist in product inventory`, true);
+        }
+      } catch (error) {
+        showMessage(`Something went wrong, Please try again.${error}`, true);
+      }
+    })
     .catch((error) => console.log("Error message: ", error));
 }
 
@@ -26,4 +40,33 @@ function showMessage(message, isError = false, isLoading = false) {
 
   if (isError) messageArea.classList.add("error");
   if (isLoading) messageArea.classList.add("loading");
+}
+
+function clearMessage() {
+  messageArea.textContent = "";
+  messageArea.classList = "message";
+}
+
+function displayProducts(products, name) {
+  clearMessage();
+  if (!products || products.length === 0) {
+    showMessage("No products to display");
+    return;
+  }
+
+  products.forEach((product) => {
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipe-item");
+    if (
+      product["name"].toLowerCase().includes(name) ||
+      product["categories"].toLowerCase().includes(name)
+    ) {
+      console.log(product.name);
+      recipeDiv.innerHTML = `
+                <img src="${placeholderImage}" alt="placeholder" loading="lazy"> 
+                <h3>${product.name}</h3>
+            `;
+      resultsGrid.appendChild(recipeDiv);
+    }
+  });
 }
